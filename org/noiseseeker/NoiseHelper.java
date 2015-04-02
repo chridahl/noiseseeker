22be30193426a0c30e49d175e934821d02e6b132
christofferdahl <christoffer.dahl@gmail.com>
2015-04-02 23:13:01 +0200

    Small changes 2

--- org/noiseseeker/NoiseHelper.java
+++ org/noiseseeker/NoiseHelper.java
@@ -55,11 +55,13 @@ public class NoiseHelper
      *
      * @return
      */
-    public Integer[][] getCurrentBuffer() {
+    public Integer[][] getCurrentBuffer()
+    {
         Integer[][] arrayTemp = new Integer[16][16];
 
         for (int y = 0; y < numberOfCells; y++)
-            for (int x = 0; x < numberOfCells; x++) {
+            for (int x = 0; x < numberOfCells; x++)
+            {
                 arrayTemp[x][y] = getBitFromCells(y, x);
             }
 
@@ -99,8 +101,10 @@ public class NoiseHelper
     public void printBuffer(Integer[][] buffer)
     {
         for (int y = 0; y < numberOfCells; y++)
-            for (int x = 0; x < numberOfCells; x++) {
+            for (int x = 0; x < numberOfCells; x++)
+            {
                 System.out.print(buffer[x][y] + "");
+                
                 if ( x == numberOfCells - 1)
                     System.out.println("");
             }
